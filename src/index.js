30bc6e67f2df82b7f37f438eeb8be75a96ce64bd
Yurii Skrypal <yurii.skrypal@vostokgames.com>
2022-08-09 18:17:34 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const BRACKETS_PAIR = Object.fromEntries(bracketsConfig.map((value) => value.reverse()));
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+
+    if ((BRACKETS_PAIR[currentSymbol] === topElement) && (stack.length > 0) ) {
+      stack.pop();
+    } else {
+      stack.push(currentSymbol);
+      }
+    }
+    return stack.length === 0;
 }
