a1d5585eb50bb91e74a31c039aecd9d5f6c15839
Yurii Skrypal <yurii.skrypal@vostokgames.com>
2022-08-09 20:24:43 +0300

    fix: test works

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
 
-  const BRACKETS_PAIR = Object.fromEntries(bracketsConfig.map((value) => value.reverse()));
-
-  let stack = [];
-
-  for (let i = 0; i < str.length; i++) {
-    let currentSymbol = str[i];
-    let topElement = stack[stack.length - 1];
-
-    if ((BRACKETS_PAIR[currentSymbol] === topElement) && (stack.length > 0) ) {
-      stack.pop();
-    } else {
-      stack.push(currentSymbol);
-      }
+str = str.split('');
+for (let i = 0; i <= str.length; i++) {
+  bracketsConfig.map(value => {
+    if (str[i] === value[0] && str[i + 1] === value[1]) {
+      str.splice(i, 2);
+      i = i - 2;
     }
-    return stack.length === 0;
+  });
+}
+
+return str.length === 0;
+
 }
